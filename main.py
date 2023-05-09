97f5aa367e2f59123bb424939addeac16f7707f8
Raymond <raydevries76@gmail.com>
2023-05-09 15:42:49 +0200

    comments update

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template      
+# this is the main file that will run the flask app
+# here we define the routes and the view functions
+# we also import the render_template function from flask
+# to render the html templates
 
-app = Flask(__name__)
+from flask import Flask, render_template   # We imported render_template
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+app = Flask(__name__)  # We created a Flask instance called app
+
+@app.route("/") # We created a route decorator
+def home(): # We created a view function
+    return render_template("home.html") # We returned a string
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/about") # We created a route decorator
+def about(): # We created a view function
+    return render_template("about.html") # We returned a string
     
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": # We used this to run the Flask application only if the script is directly executed from the Python interpreter and not used as an imported module
+    app.run(debug=True) # We ran the Flask app in debug mode
     
-   # We made two new changes
